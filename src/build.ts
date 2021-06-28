78e6bf936e97cd972e880bf49c4affea33c5b20c
Prokop Schield <prokop.schield@protonmail.com>
2021-06-28 15:44:29 +0200

    1.0.10 Build using nsmt-nslibmgr

--- src/build.ts
+++ src/build.ts
@@ -1,28 +1,8 @@
-import { exec } from 'child_process';
-
-async function run (command: string): Promise<void> {
-	return new Promise(resolve => {
-		let resolved = false;
-		const child = exec(command, (error, stdout, stderr) => {
-			resolved = true;
-			resolve();
-		});
-		if (child && child.stdin && child.stdout && child.stderr) {
-			child.stdout.on('data', (data: Buffer) => {
-				if (!resolved && data.toString('utf8').includes('Ok to proceed')) {
-					child && child.stdin && child.stdin.write('y\n');
-				}
-			});
-			child.stdout.pipe(process.stdout);
-			child.stderr.pipe(process.stdout);
-		}
-	});
-}
+import run from 'nsmt-nslibmgr/lib/run';
 
 (async () => {
-	await run("npx yarn");
-	await run("tsc --module CommonJS --target ES5 --declaration --esModuleInterop --strict --outDir lib/ src/*");
-	await run("npx browserify lib/index.js -o lib/bundle.js");
-	await run("npx js-beautify lib/bundle.js >lib/io.js");
-	await run("npx minify lib/io.js >lib/min.js");
+	await run('npx nsmt-nslibmgr make clean');
+	await run('npx browserify lib/index.js -o lib/bundle.js');
+	await run('npx nsmt-nslibmgr lint');
+	await run('npx minify lib/bundle.js >lib/min.js');
 })();
